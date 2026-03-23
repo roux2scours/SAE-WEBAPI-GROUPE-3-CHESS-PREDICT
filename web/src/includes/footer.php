6247ce3287d652f232e6c09f18ab596eec6f9b79
arachidd <derooraphael077@gmail.com>
2026-03-23 16:30:16 +0100

    add(style): footer

--- web/src/includes/footer.php
+++ web/src/includes/footer.php
@@ -4,6 +4,7 @@ require_once 'config.php';
 
 <footer>
     <div class="footer-links">
-        <a href=<?php echo GITHUB_URL; ?>></a>
+        <a href=<?php echo GITHUB_URL; ?> class="contact">Voir le github</a>
+        <a href="">Mentions légales</a>
     </div>
 </footer>
--- web/src/index.php
+++ web/src/index.php
@@ -4,4 +4,7 @@ require_once 'config.php';
 include 'includes/header.php';
 include 'includes/nav.php';
 
+
 echo "<h1>SAÉ BUT2 – Application Web</h1>";
+
+include 'includes/footer.php';
