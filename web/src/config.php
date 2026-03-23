aebd608b72b8e32b1fc4ec449c5818c503704cac
egn-thomas <thomas.eugene.62250@gmail.com>
2026-03-23 15:53:46 +0100

    add : link to github page

--- web/src/config.php
+++ web/src/config.php
@@ -3,5 +3,6 @@
 define('SITE_NAME', 'Chess.preview');
 define('LOGO_URL', 'assets/images/logo.svg');
 define('CURRENT_PAGE', 'Leaderboard');
+define('GITHUB_URL', 'https://github.com/roux2scours/SAE-WEBAPI-GROUPE-3-CHESS-PREDICT.git');
 
 ?>
