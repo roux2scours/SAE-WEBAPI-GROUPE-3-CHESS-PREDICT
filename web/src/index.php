36bf84944c48b953014aa4031e49e190fe0c788c
egn-thomas <thomas.eugene.62250@gmail.com>
2026-03-25 12:54:51 +0100

    add: improve routing

--- web/src/index.php
+++ web/src/index.php
@@ -4,19 +4,26 @@ require_once 'includes/functions.php';
 
 $page = get_page();
 
-// Inclure le header et la navigation
-include 'includes/header.php';
-include 'includes/nav.php';
-
 // Router - Charger la bonne page
 switch ($page) {
     case 'test':
         include 'pages/test.php';
+        $page_name = "Page de test";
         break;
     case 'game':
         include 'pages/game.php';
+        $page_name = "Voir une partie";
+        break;
+    default :
+        include 'pages/leaderboard.php';
+        $page_name = "Leaderboard";
         break;
 }
+
+// Inclure le header et la navigation
+include 'includes/header.php';
+include 'includes/nav.php';
+
 
 // Inclure le footer
 include 'includes/footer.php';
